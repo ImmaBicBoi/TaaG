aecc93cbf12e85faf1b51b1d85add276d63e66c0
carlos <carlos.urena@student.fairfield.edu>
2018-10-09 18:40:30 -0400

    added currentID getter/setter

--- assets/js/positions.js
+++ assets/js/positions.js
@@ -1,6 +1,8 @@
 //POSITIONS.JS - For JavaScript Functions that are directly related to positions.
 
 function loadPositions() {
+
+    
     //Removes any Existing List Items for the update
     $('#position-list li').remove();
     $.getJSON('mockdata/mock_positions.json', function (data) {
@@ -15,10 +17,12 @@ function loadPositions() {
                                     $('#details-title').html(data[i].pos_name); //insert position title 
                                     $('#pos-heldby').html("<span class='modal-headers'>Position Held By: </span>" + "<p id = 'pos-ocname'contenteditable='false'>" + data[i].pos_occupant_first_name + " " + data[i].pos_occupant_last_name) + "</p>"; //insert position heldby name
                                     $('#pos-weight').html("<span class='modal-headers'>Position Weight:</span>" + "<p id = 'pos-weightdata' contenteditable='false'>" + data[i].pos_weight + "</p>"); //insert positon weight
-                                    $('#pos-attributes').html(""); //insert positon weight
+                                    $('#pos-attributes').html(""); //insert position attributes
                                     $('#pos-attributes').append("<span class='modal-headers'>Key 1:</span>" + "<p id ='Value1' contenteditable='false'>Value 1</p>"); //insert positon adittional attributes
                                     $('#pos-attributes').append("<span class='modal-headers'>Key 2:</span>" + "<p id ='Value2' contenteditable='false'>Value 2</p>"); //insert positon adittional attributes
                                     $('#pos-attributes').append("<span class='modal-headers'>Key 3</span>" + "<p id ='Value3' contenteditable='false'>Value 3</p>"); //insert positon adittional attributes
+                                    
+                                    setCurrentID(data[i].id);
                                     document.getElementById('edit-btn').style = "display: block;" //show EDIT button
                                     document.getElementById('save-btn').style = "display: none;" //hide save button
 
@@ -28,6 +32,7 @@ function loadPositions() {
         });
     });
     console.log('positions loaded.');
+    
 };
 
 
