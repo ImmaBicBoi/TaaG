95b1056076b20ba46610441aea6091a8b0d0b8e1
Michael Donofrio <michael.a.donofrio@hotmail.com>
2018-11-20 16:52:16 -0500

    Delete MxGraph Elements

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -19,7 +19,57 @@ posToolbar.enabled = false;
 var personToolbar = new mxToolbar(tbPersonContainer);
 personToolbar.enabled = false;
 
+// Defines a new class for all icons
+function mxIconSet(state)
+{
+	this.images = [];
+	var graph = state.view.graph;
 
+	
+	// Delete
+	var img = mxUtils.createImage('images/delete.png');
+	img.setAttribute('title', 'Delete');
+	img.style.position = 'absolute';
+	img.style.cursor = 'pointer';
+	img.style.width = '16px';
+	img.style.height = '16px';
+	img.style.left = (state.x + state.width) + 'px';
+	img.style.top = (state.y - 16) + 'px';
+	
+	mxEvent.addGestureListeners(img,
+		mxUtils.bind(this, function(evt)
+		{
+			// Disables dragging the image
+			mxEvent.consume(evt);
+		})
+	);
+	
+	mxEvent.addListener(img, 'click',
+		mxUtils.bind(this, function(evt)
+		{
+			graph.removeCells([state.cell]);
+			mxEvent.consume(evt);
+			this.destroy();
+		})
+	);
+	
+	state.view.graph.container.appendChild(img);
+	this.images.push(img);
+};
+
+mxIconSet.prototype.destroy = function()
+{
+	if (this.images != null)
+	{
+		for (var i = 0; i < this.images.length; i++)
+		{
+			var img = this.images[i];
+			img.parentNode.removeChild(img);
+		}
+	}
+	
+	this.images = null;
+};
 
 function initializeGraph(container){
             // -------------------- mxGraph initialization ------------------------------------ //
@@ -70,25 +120,82 @@ function initializeGraph(container){
 				// Enables new connections in the graph
 				graph.setConnectable(true);
 				graph.setMultigraph(false);
+
+				
+				// Defines the tolerance before removing the icons
+				var iconTolerance = 20;
+				// Shows icons if the mouse is over a cell
+				graph.addMouseListener(
+				{
+					currentState: null,
+					currentIconSet: null,
+					mouseDown: function(sender, me)
+					{
+						// Hides icons on mouse down
+						if (this.currentState != null)
+						{
+							this.dragLeave(me.getEvent(), this.currentState);
+							this.currentState = null;
+						}
+					},
+					mouseMove: function(sender, me)
+					{
+						if (this.currentState != null && (me.getState() == this.currentState ||
+							me.getState() == null))
+						{
+							var tol = iconTolerance;
+							var tmp = new mxRectangle(me.getGraphX() - tol,
+								me.getGraphY() - tol, 2 * tol, 2 * tol);
+							if (mxUtils.intersects(tmp, this.currentState))
+							{
+								return;
+							}
+						}
+						
+						var tmp = graph.view.getState(me.getCell());
+						
+						// Ignores everything but vertices
+						// if (graph.isMouseDown || (tmp != null && !graph.getModel().isVertex(tmp.cell)))
+						// {
+						// 	tmp = null;
+						// }
+						if (tmp != this.currentState)
+						{
+							if (this.currentState != null)
+							{
+								this.dragLeave(me.getEvent(), this.currentState);
+							}
+						
+							this.currentState = tmp;
+						
+							if (this.currentState != null)
+							{
+								this.dragEnter(me.getEvent(), this.currentState);
+							}
+						}
+					},
+					mouseUp: function(sender, me) { },
+					dragEnter: function(evt, state)
+					{
+						if (this.currentIconSet == null)
+						{
+							this.currentIconSet = new mxIconSet(state);
+						}
+					},
+					dragLeave: function(evt, state)
+					{
+						if (this.currentIconSet != null)
+						{
+							this.currentIconSet.destroy();
+							this.currentIconSet = null;
+						}
+					}
+				});
+				
+
 				// Stops editing on enter or escape keypress
 				var keyHandler = new mxKeyHandler(graph);
-				var rubberband = new mxRubberband(graph);
-				
-				
-				
-                
-                //UNCOMMENT HERE
-				// addVertex($('#position-list').append('<div></div>'), 120, 160, 'shape=rounded;', 23, "yoo");
-				// addVertex($('#position-list').append('<div></div>'), 100, 40, '', 345, "ewds");
-				// addVertex('editors/images/rounded.gif', 100, 40, 'shape=rounded');
-				// addVertex('editors/images/ellipse.gif', 40, 40, 'shape=ellipse');
-				// addVertex('editors/images/rhombus.gif', 40, 40, 'shape=rhombus');
-				// addVertex('editors/images/triangle.gif', 40, 40, 'shape=triangle');
-				// addVertex('editors/images/cylinder.gif', 40, 40, 'shape=cylinder');
-				// addVertex('editors/images/actor.gif', 30, 40, 'shape=actor');
-				// toolbar.addLine();
-				
-				
+				var rubberband = new mxRubberband(graph);			
 																	
 			}
         }
@@ -163,8 +270,6 @@ function addPositionToolbarItem(graph, posToolbar, prototype, image, data, key)
 			});
 			$(img).addClass("draggable-list-button");
 		}
-
-
 
 function addPersonToolbarItem(graph, personToolbar, prototype, image, data, key)
 {
