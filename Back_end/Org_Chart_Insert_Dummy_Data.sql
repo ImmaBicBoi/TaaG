918f3f36334039765b1165e91f7aa4387144b184
Unknown <bindumadhavi.pesaladinne@student.fairfield.edu>
2018-12-10 20:48:25 -0500

    attributes fix

--- Back_end/Org_Chart_Insert_Dummy_Data.sql
+++ Back_end/Org_Chart_Insert_Dummy_Data.sql
@@ -55,17 +55,56 @@ create table POSITION_ATTRIBUTE (
 insert into POSITION_ATTRIBUTE (POS_ATTR_KEY,POS_ATTR_VALUE,POSITION_ID) values 
 ('Type','Full Time',1),
 ('Base Pay','$ 250,000',1),
-('Type','Full Time',2),
+('Years of experience','8 years',1),
+('Programming Language skill','Java',1),
+
+('Type','Part Time',2),
+('Base Pay','$ 50,000',2),
+('Years of experience','9 years',2),
+('Programming Language skill','C#',2),
+
+
 ('Type','Full Time',3),
-('Type','Full Time',4),
+('Base Pay','$ 250,000',3),
+('Years of experience','8 years',3),
+('Programming Language skill','Java',3),
+
+('Type','Part Time',4),
+('Base Pay','$ 350,000',4),
+('Years of experience','10 years',4),
+('Programming Language skill','Java',4),
+
 ('Type','Full Time',5),
-('Type','Full Time',6),
-('Years of experience','8 years',6),
-('Type','Full Time',7),
-('Programming Language skill','Java',7),
-('Type','Full Time',8),
-('Type','Full Time',9),
-('Type','Part Time',10);
+('Base Pay','$ 250,000',5),
+('Years of experience','8 years',5),
+('Programming Language skill','Java',5),
+
+('Type','',6),
+('Base Pay','',6),
+('Years of experience','',6),
+('Programming Language skill','',6),
+
+('Type','',7),
+('Base Pay','',7),
+('Years of experience','',7),
+('Programming Language skill','',7),
+
+('Type','',8),
+('Base Pay','',8),
+('Years of experience','',8),
+('Programming Language skill','',8),
+
+('Type','',9),
+('Base Pay','',9),
+('Years of experience','',9),
+('Programming Language skill','',9),
+
+('Type','',10),
+('Base Pay','',10),
+('Years of experience','',10),
+('Programming Language skill','',10);
+
+
 
 /******************************************************/
 /**************       PERSON            ***************/
@@ -117,16 +156,66 @@ create table PERSON_ATTRIBUTE (
 insert into PERSON_ATTRIBUTE (PER_ATTR_KEY,PER_ATTR_VALUE,PERSON_ID ) values 
 ('Email','frank.ellison@gmail.com',1),
 ('Office','Fairfield, CT',1),
+('Address','Main Ave, Stamford', 1),
+('Mobile Number','728-584-7730', 1),
+('DOB','06-09-1987', 1),
+
 ('Email','sarah.mitchell@gmail.com',2),
+('Office','Fairfield, CT',2),
+('Address','Main Ave, Stamford', 2),
+('Mobile Number','728-500-7700', 2),
+('DOB','07-08-1987', 6),
+
+('Email','rachel.green@gmail.com', 3),
+('Office','Fairfield, CT',3),
 ('Address','Main Ave, Stamford', 3),
-('Email','rachel.green@gmail.com', 4),
+('Mobile Number','728-584-7730', 3),
+('DOB','06-09-1987', 3),
+
+
+('Email','peter.clark@gmail.com', 4),
+('Office','Fairfield, CT',4),
+('Address','Main Ave, Stamford', 4),
+('Mobile Number','728-584-7730', 4),
+('DOB','06-09-1987', 4),
+
+('Email','jason.murray@gmail.com', 5),
+('Office','Fairfield, CT',5),
+('Address','Belden Ave, Fairfield',5),
 ('Mobile Number','728-584-7730', 5),
-('DOB','06-09-1987', 6),
-('Email','peter.clark@gmail.com', 7),
-('Contact Number','257-317-9134', 7),
-('DOB','3-02-1990', 8),
-('Address','Belden Ave, Fairfield',9),
-('Email','jason.murray@gmail.com', 10);
+('DOB','3-02-1990', 5),
+
+('Email','',6),
+('Office','',6),
+('Address','', 6),
+('Mobile Number','', 6),
+('DOB','', 6),
+
+('Email','',8),
+('Office','',8),
+('Address','', 8),
+('Mobile Number','', 8),
+('DOB','', 8),
+
+('Email','', 7),
+('Office','',7),
+('Address','', 7),
+('Mobile Number','', 7),
+('DOB','', 7),
+
+
+('Email','', 9),
+('Office','',9),
+('Address','', 9),
+('Mobile Number','', 9),
+('DOB','', 9),
+
+('Email','', 10),
+('Office','',10),
+('Address','',10),
+('Mobile Number','', 10),
+('DOB','', 10);
+
 
 /******************************************************/
 
