d4191e5ec3ce9ce31808785e641c57abca35c5f5
Unknown <bindumadhavi.pesaladinne@student.fairfield.edu>
2018-12-10 22:08:18 -0500

    save message fix

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -301,7 +301,7 @@ function saveGraph(graphData){
         //alert(data + " " + status);
 		if(jqXHR.status == 200){
       var div = document.getElementById('messages');
-      div.innerHTML +=" ";
+      document.getElementById("messages").innerHTML = "";
       div.innerHTML += 'Chart Saved Succesfully';
     }
         console.log("response "+JSON.stringify(data) + " " + jqXHR.status);
